6b121609c82b425a462e87188cc8269dec2ade89
vanineveld <42658008+vanineveld@users.noreply.github.com>
2022-03-16 11:07:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function menu() {
+	document.getElementById("top").style.transform = "rotate(-45deg)";
+	document.getElementById("top").style.marginBottom = "0";
+	document.getElementById("bottom").style.transform = "rotate(45deg)";
+}
